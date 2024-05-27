685ce3e6c4f0df2c3923cce5547fe91542d800d8
Alexis <samanthajimenez186@gmail.com>
2024-05-26 21:20:03 -0600

    miau progresión.
    Co-authored-by: Brit1019 <Brit1019@users.noreply.github.com>

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ var header = document.getElementById(Header)
 window.addEventListener(scroll, ()=>{
     var scroll = window.scrollY
     if(scroll>10){
-    header.style.backgroundColor = "#121212"
+    header.style.backgroundColor = "#FFFAFA"
     }else {
         header.style.backgroundColor = transparent
     }
