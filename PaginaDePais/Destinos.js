6dfef5190c18d7ef6082c73a1c087023465095fe
Alexis <samanthajimenez186@gmail.com>
2024-11-03 19:12:28 -0600

    ,

--- PaginaDePais/Destinos.js
+++ PaginaDePais/Destinos.js
@@ -1,36 +1,36 @@
 const Destinos = {
     Bélgica: {
-        ImagenPais: 'url("../ImagenesDestinos/Belgica/Belgica.jpg")',
+        ImagenPais: '',
         DescripcionPais: 'Un pequeño país con un inmenso corazón europeo, donde cada ciudad revela una historia, sabores irresistibles y un arte que deslumbra',
-        Bandera: '../ImagenesDestinos/Japon/Shibuya.jpg',
+        Bandera: '',
         Amberes: {
             Nombre: 'Amberes',
-            Imagen: 'url(../ImagenesDestinos/Belgica/Amberes.jpg)',
+            Imagen: '',
             Descripcion: 'Amberes, la capital mundial de los diamantes, deslumbra con su impresionante puerto, su catedral gótica y su vibrante escena artística.',
         },
         Dinant: {
             Nombre: 'Dinant',
-            Imagen: 'url("../ImagenesDestinos/Belgica/Amberes.jpg")',
+            Imagen: '',
             Descripcion: 'Emplazada a orillas del Mosa, Dinant te cautivará con sus imponentes acantilados, la majestuosa ciudadela y el legado del jazz de Adolphe Sax.',
         },
         Brujas: {
             Nombre: 'Brujas',
-            Imagen: 'url("../ImagenesDestinos/Belgica/Brujas.jpg")',
+            Imagen: '',
             Descripcion: 'Sumérgete en la magia medieval de Brujas, con sus canales románticos, calles empedradas y encantadores puentes que invitan a perderse en el tiempo',
         },
         Bruselas: {
             Nombre: 'Bruselas',
-            Imagen: 'url("../ImagenesDestinos/Belgica/Bruselas.jpg")',
+            Imagen: '',
             Descripcion: 'Descubre la vibrante capital de Europa, donde la arquitectura gótica se mezcla con la modernidad en una ciudad llena de arte, cultura y exquisita gastronomía."',
         },
         Lieja: {
             Nombre: 'Lieja',
-            Imagen: 'url("../ImagenesDestinos/Belgica/Lieja.jpg")',
+            Imagen: '',
             Descripcion: 'Lieja, la ardiente ciudad valona, te espera con su rica historia industrial, su dinamismo cultural y su deliciosa cocina regional',
         },
         Lovaina: {
             Nombre: 'Lovaina',
-            Imagen: 'url("../ImagenesDestinos/Belgica/Lovaina.jpg")',
+            Imagen: '',
             Descripcion: 'Vive el ambiente estudiantil en Lovaina, hogar de una de las universidades más antiguas de Europa y famosa por su vibrante vida nocturna y cervezas artesanales',
         }
     
