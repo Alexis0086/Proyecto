4bd2db9b8a5275b43b7cd5db0ea589db58937fe5
Alexis <samanthajimenez186@gmail.com>
2024-11-04 18:20:51 -0600

    Aún chambeando

--- PaginaDePais/Destinos.js
+++ PaginaDePais/Destinos.js
@@ -7,31 +7,37 @@ const Destinos = {
             Nombre: 'Amberes',
             Imagen: 'url("../ImagenesDestinos/Belgica/Amberes.jpg")',
             Descripcion: 'Amberes, la capital mundial de los diamantes, deslumbra con su impresionante puerto, su catedral gótica y su vibrante escena artística.',
+            Panoramica: '<iframe width="90%" height="500px" allowFullScreen="true" allow="accelerometer; magnetometer; gyroscope" style="display:block; margin:20px auto; border:0 none; max-width:880px;border-radius:8px; box-shadow: 0 1px 1px rgba(0,0,0,0.11),0 2px 2px rgba(0,0,0,0.11),0 4px 4px rgba(0,0,0,0.11),0 6px 8px rgba(0,0,0,0.11),0 8px 16px rgba(0,0,0,0.11);" src="https://panoraven.com/es/embed/5qmHNSQPsW"></iframe>',
         },
         Dinant: {
             Nombre: 'Dinant',
             Imagen: 'url("../ImagenesDestinos/Belgica/Dinant.jpg")',
             Descripcion: 'Emplazada a orillas del Mosa, Dinant te cautivará con sus imponentes acantilados, la majestuosa ciudadela y el legado del jazz de Adolphe Sax.',
+            Panoramica: '<iframe width="90%" height="500px" allowFullScreen="true" allow="accelerometer; magnetometer; gyroscope" style="display:block; margin:20px auto; border:0 none; max-width:880px;border-radius:8px; box-shadow: 0 1px 1px rgba(0,0,0,0.11),0 2px 2px rgba(0,0,0,0.11),0 4px 4px rgba(0,0,0,0.11),0 6px 8px rgba(0,0,0,0.11),0 8px 16px rgba(0,0,0,0.11);" src="https://panoraven.com/es/embed/hPOvRvWdb1"></iframe>',
         },
         Brujas: {
             Nombre: 'Brujas',
             Imagen: 'url("../ImagenesDestinos/Belgica/Brujas.jpg")',
             Descripcion: 'Sumérgete en la magia medieval de Brujas, con sus canales románticos, calles empedradas y encantadores puentes que invitan a perderse en el tiempo',
+            Panoramica: '<iframe width="90%" height="500px" allowFullScreen="true" allow="accelerometer; magnetometer; gyroscope" style="display:block; margin:20px auto; border:0 none; max-width:880px;border-radius:8px; box-shadow: 0 1px 1px rgba(0,0,0,0.11),0 2px 2px rgba(0,0,0,0.11),0 4px 4px rgba(0,0,0,0.11),0 6px 8px rgba(0,0,0,0.11),0 8px 16px rgba(0,0,0,0.11);" src="https://panoraven.com/es/embed/hPOvRvWdb1"></iframe>',
         },
         Bruselas: {
             Nombre: 'Bruselas',
             Imagen: 'url("../ImagenesDestinos/Belgica/Bruselas.jpg")',
             Descripcion: 'Descubre la vibrante capital de Europa, donde la arquitectura gótica se mezcla con la modernidad en una ciudad llena de arte, cultura y exquisita gastronomía."',
+            Panoramica: '',
         },
         Lieja: {
             Nombre: 'Lieja',
             Imagen: 'url("../ImagenesDestinos/Belgica/Lieja.jpg")',
             Descripcion: 'Lieja, la ardiente ciudad valona, te espera con su rica historia industrial, su dinamismo cultural y su deliciosa cocina regional',
+            Panoramica: '<iframe width="90%" height="500px" allowFullScreen="true" allow="accelerometer; magnetometer; gyroscope" style="display:block; margin:20px auto; border:0 none; max-width:880px;border-radius:8px; box-shadow: 0 1px 1px rgba(0,0,0,0.11),0 2px 2px rgba(0,0,0,0.11),0 4px 4px rgba(0,0,0,0.11),0 6px 8px rgba(0,0,0,0.11),0 8px 16px rgba(0,0,0,0.11);" src="https://panoraven.com/es/embed/hPOvRvWdb1"></iframe>',
         },
         Lovaina: {
             Nombre: 'Lovaina',
             Imagen: 'url("../ImagenesDestinos/Belgica/Lovaina.jpg")',
             Descripcion: 'Vive el ambiente estudiantil en Lovaina, hogar de una de las universidades más antiguas de Europa y famosa por su vibrante vida nocturna y cervezas artesanales',
+            Panoramica: '<iframe width="90%" height="500px" allowFullScreen="true" allow="accelerometer; magnetometer; gyroscope" style="display:block; margin:20px auto; border:0 none; max-width:880px;border-radius:8px; box-shadow: 0 1px 1px rgba(0,0,0,0.11),0 2px 2px rgba(0,0,0,0.11),0 4px 4px rgba(0,0,0,0.11),0 6px 8px rgba(0,0,0,0.11),0 8px 16px rgba(0,0,0,0.11);" src="https://panoraven.com/es/embed/hPOvRvWdb1"></iframe>',
         }
     
     },
@@ -54,6 +60,20 @@ const Destinos = {
             Nombre: 'Osaka',
             Imagen: 'url("../ImagenesDestinos/Japon/Namba osaka (no 360).jpg")',
             Descripcion: 'Luces, bullicio y diversión: Osaka es un lugar de diversión y entretenimiento con pocas pretensiones.',
-        }
+        },
+        
+        Kanagawa:{
+            Nombre: 'Kanagawa'
+        },
+
+        // Dale.viste como las puse yo. Agregue otra llave al objeto llamada "Panoramica" ponlo así ti tmb Oki, pero ahorita. Igual también debería en un rato hacer lo la parte escrita de todos nosotros, porque toca investigación. Pero no lo hagas tu diles a ellos que busquen, tu nada mas les pasas los datos que ocupas de cada lugar as.i como lo hice yo Sí, seguro ahorita lo pongo, para darles un ejemplo de como sería, porque es bastante. utiliza los objetos para acomodarlo pq de eso depende que todo se automatico y no hacer un HTML para cada pais ya vengo voy a tomar un descnaso OKi.
+
+        Nara:{
+            Nombre: 'Nara'
+        },
+
+        Hiroshima:{
+            Nombre: 'Hiroshima'
+        },
     },
 }
